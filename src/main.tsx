42c540aab62cbbada0e9e7376274736fa276c903
Dev9Days <dev9days@gmail.com>
2026-07-01 15:43:36 +0900

    Move app to custom domain route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import { installCloudflareAnalytics } from "./utils/cloudflareAnalytics";
+import { normalizeAppRoute } from "./utils/routing";
 
+normalizeAppRoute();
 createRoot(document.getElementById("root")!).render(<App />);
 installCloudflareAnalytics();
 
---
+++ src/utils/routing.ts
@@ -0,0 +1,11 @@
+export const CANONICAL_APP_PATH = "/emerald/cheats";
+
+const LEGACY_APP_PATHS = new Set(["/", "/pokemon-emerald-cheats", "/pokemon-emerald-cheats/", "/emerald/cheats/"]);
+
+export function normalizeAppRoute() {
+  const { hash, pathname, search } = window.location;
+
+  if (!LEGACY_APP_PATHS.has(pathname)) return;
+
+  window.history.replaceState(null, "", `${CANONICAL_APP_PATH}${search}${hash}`);
+}
